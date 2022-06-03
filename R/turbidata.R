5df201a380c18f7dfcc4fc74a7c8e9f4013c2955
riedam <104423964+riedam@users.noreply.github.com>
2022-06-03 17:26:27 +0200

    Update turbidata class and documentation

--- R/turbidata.R
+++ R/turbidata.R
@@ -4,8 +4,9 @@
 turbidata <- R6::R6Class(
   "TurbiDumpData",
   private = list(
-    ..data = NA,
+    ..data = data.frame(),
     ..signal = c(".T", ".R"),
+    ..name = character(),
 
     shared = {
       env <- new.env()
@@ -42,7 +43,7 @@ turbidata <- R6::R6Class(
     #' @description Allows you to calculate on the relative values of the data.frame
     #' @param r (optional) the time used as reference for the backscatter (default: \code{1})
     #' @param t (optional) the time used as reference for the transmission (default: \code{1})
-    #' @return A new \code{TurbiRelativeData} object.
+    #' @return A new [TurbiRelativeData][turbidata::relative_turbidata] object.
     use_relative = function(r = 1, t = 1) {
       reference <-  c(".T" = t, ".R" = r)
       relative <-
@@ -57,20 +58,32 @@ turbidata <- R6::R6Class(
     #' @description Allows you to calculate on the absolute values of the data.frame
     #' @param r (optional) the time used as reference for the backscatter (default: \code{1})
     #' @param t (optional) the time used as reference for the transmission (default: \code{1})
-    #' @return A new \code{TurbiAbsoluteData} object.
+    #' @return A new [TurbiAbsoluteData][turbidata::relative_turbidata] object.
     use_absolute = function(r = 1, t = 1) {
       absolute <- turbidata:::.pivot_longer(private$..data)
       return(turbidata::.absolute_turbidata$new(absolute, private$..signal))
     }
 
+  ),
+  active = list(
+    #' @field name \code{character} The name of the sample
+    name = function(value) {
+      if (missing(value))
+        return(private$..name)
+      else {
+        stopifnot(is.character(value))
+        private$..name <- value
+      }
+    }
   )
 )
 
 #' TurbiRelativeData
 #' @description
 #' R6 Class representing the data convert in relative value.
-#' Do not use $new() of this class directly : use \code{TurbiDumpData$use_relative()}
-.relative_turbidata <- R6::R6Class(
+#' @section Warning:
+#' Do not use $new() of this class directly : use [TurbiDumpData$use_relative()][turbidata::turbidata]
+relative_turbidata <- R6::R6Class(
   "TurbiRelativeData",
   inherit = turbidata,
   public = list(
@@ -90,8 +103,9 @@ turbidata <- R6::R6Class(
 #' TurbiAbsoluteData
 #' @description
 #' R6 Class representing the data convert in relative value.
-#' Do not use $new() of this class directly : use \code{TurbiDumpData$use_absolute()}
-.absolute_turbidata <- R6::R6Class(
+#' @section Warning:
+#' Do not use $new() of this class directly : use [TurbiDumpData$use_absolute()][turbidata::turbidata]
+absolute_turbidata <- R6::R6Class(
   "TurbiAbsoluteData",
   inherit = turbidata,
   public = list(
