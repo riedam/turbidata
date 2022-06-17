199189a4a04e0848089479992a66bad3b93b1a20
riedam <104423964+riedam@users.noreply.github.com>
2022-06-17 11:28:56 +0200

    Delete data because it break instalation (with cache directory)

--- R/data.R
+++ R/data.R
@@ -1,9 +1,1 @@
-#' An object belonging to the Turbidata class
-#' @format data$data
-#' \describe{
-#'   \item{height}{height, in cm}
-#'   \item{time}{time, in min}
-#'   \item{signal}{signal, can be T or R}
-#'   \item{do}{the value}
-#' }
-"test_data"
+
