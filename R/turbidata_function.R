f6529501e2b953908edd89986aeb43246b87f302
riedam <104423964+riedam@users.noreply.github.com>
2022-09-08 02:05:05 +0200

    Add turbidata_version_check

--- R/turbidata_function.R
+++ R/turbidata_function.R
@@ -1,7 +1,26 @@
+#' turbidata_version_check
+#' @description Check if package are up to date
+#' @return NULL
+#' @export
+turbidata_version_check <- function() {
+  curent <- as.character(utils::packageVersion('turbidata'))
+
+  request <- httr::GET('https://api.github.com/repos/riedam/turbidata/releases',  httr::accept_json())
+  if (request$status_code != 200) {stop('http error ', request$status_code)}
+
+  latest <-  httr::content(request)[[1]]$tag_name
+
+  if (utils::compareVersion(latest, curent) > 0) {
+    cat('New version available! Curent version : ', curent, ', Latest version: ', latest, sep = '')
+  } else {
+    cat('You are up to date, curent version :', curent)
+  }
+}
+
 #' turbidata
 #' @description turbidata() : Wrapper for [Turbidata class][turbidata::turbidata_class]
 #' @param ... Argument transfert to turbidata_class$new()
 #' @export
 turbidata <- function(...) {
-  turbidata:::turbidata_class$new(...)
+  turbidata_class$new(...)
 }
